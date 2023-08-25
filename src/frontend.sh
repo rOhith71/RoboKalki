2549e6e4caec3fcaf933b3f976c41540bf8ed7ed
Rohith <your_email@whatever.com>
2023-08-25 08:07:43 -0400

    Specnnt -5646 : Initial commit config

--- src/frontend.sh
+++ src/frontend.sh
@@ -6,9 +6,10 @@ systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp .roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
 systemctl enable nginx
 #good job
