191fa8459f3ce941be5c5d527eb0ab39daa96f4a
Rohith <your_email@whatever.com>
2023-08-26 07:29:11 -0400

    Specnnt -5646 : Initial commit config

--- src/frontend.sh
+++ src/frontend.sh
@@ -2,6 +2,7 @@
 #other api
 
 yum install nginx -y
+cp .roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
@@ -9,7 +10,6 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp .roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
 systemctl enable nginx
 #good job
