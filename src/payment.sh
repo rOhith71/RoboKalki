00d24dc8b3acbe2ff5db7ae3ae93064b0e020226
Rohith <your_email@whatever.com>
2023-09-18 01:17:19 -0400

    Specnnt -5646 : Initial commit

--- src/payment.sh
+++ src/payment.sh
@@ -1,6 +1,7 @@
 echo -e "\e[31m << Payment >> \e[0m"
 yum install python36 gcc python3-devel -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 cd /app
