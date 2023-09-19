214c9bc778817795e502c530c628d61cb3846920
Rohith <your_email@whatever.com>
2023-09-19 02:11:27 -0400

    Specnnt -5646 : Initial commit

--- src/mongo.sh
+++ src/mongo.sh
@@ -1,6 +1,8 @@
 echo -e "\e <<Insatlling Mongo  >> \e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
+ sed -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
-systemctl start mongod
+
 systemctl restart mongod
+systemctl start mongod
